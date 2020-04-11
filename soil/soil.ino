7e3d67078072e3ec5c03aa12b020ffa38f8a1d5c
romanjrdykyj <romanjrdykyj@gmail.com>
2020-04-11 23:38:18 +0200

    Update soil.ino
    1)problem z rozdzielczoscia (podzielilem przez 4)
    2)zrobic funkcje na %
    3) spradzic troszke przesuszona ziemie

--- soil/soil.ino
+++ soil/soil.ino
@@ -1,12 +1,21 @@
-#define sensorPin 5;
-int sensorValue = 0;
-
-void setup() {
-    Serial.begin(9600);
+int sensor_pin = 14;
+int value ;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Reading");
+  delay(2000);
 }
-void loop() {
-    sensorValue = analogRead(sensorPin);
-    Serial.print("Moisture = " );
-    Serial.println(sensorValue);
-    delay(1000);
+ 
+void loop()
+{
+ 
+  value= analogRead(sensor_pin);
+  //value=value/4;
+  value = map(value,0,4000,0,100);
+  Serial.print("Moisture : ");
+  Serial.print(value);
+  Serial.println("%");
+  delay(1000);
 }
